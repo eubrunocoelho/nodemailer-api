7bf9ce16ecc25668a135f015b7cbaf315a0fd46c
eubrunocoelho <eu.brunocoelho94@gmail.com>
2024-02-14 12:28:41 -0300

    feat: adds data validation

--- src/App.ts
+++ src/App.ts
@@ -1,4 +1,5 @@
 import express, { Express, Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 class App {
     public express: Express;
@@ -10,7 +11,19 @@ class App {
     }
 
     routes(): void {
-        this.express.route('/').get((req: Request, res: Response) => {
+        this.express.route('/send').post([
+            body('name').notEmpty().isLength({ min: 3, max: 128 }),
+            body('phone').notEmpty().matches(/^\([0-9]{2}\) [0-9]?[0-9]{4}-[0-9]{4}$/),
+            body('email').notEmpty().isLength({ max: 128 }).isEmail(),
+            body('subject').notEmpty().isLength({ min: 8, max: 128 }),
+            body('message').notEmpty().isLength({ min: 20, max: 1200 })
+        ], (req: Request, res: Response) => {
+            const errors = validationResult(req);
+
+            if (!errors.isEmpty()) {
+                return res.json({ errors: errors.array() });
+            }
+
             res.send('Olá, mundo!');
         });
     }
