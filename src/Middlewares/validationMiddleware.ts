0363f8cc83a7d78e5d5b0a0b9c0aec157b45dabf
eubrunocoelho <eu.brunocoelho94@gmail.com>
2024-02-14 16:31:31 -0300

    chore: change validation rules

--- src/Middlewares/validationMiddleware.ts
+++ src/Middlewares/validationMiddleware.ts
@@ -4,7 +4,7 @@ const validationMiddleware: ValidationChain[] = [
     body('name').notEmpty().isLength({ min: 3, max: 128 }),
     body('phone').notEmpty().matches(/^\([0-9]{2}\) [0-9]?[0-9]{4}-[0-9]{4}$/),
     body('email').notEmpty().isLength({ max: 128 }).isEmail(),
-    body('subject').notEmpty().isLength({ min: 8, max: 128 }),
+    body('subject').notEmpty().isLength({ min: 6, max: 128 }),
     body('message').notEmpty().isLength({ min: 20, max: 1200 })
 ];
 
