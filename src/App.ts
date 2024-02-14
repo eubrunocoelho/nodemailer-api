80f08489fac1f10c89ce25a10a5d7e3a55e4dab4
eubrunocoelho <eu.brunocoelho94@gmail.com>
2024-02-14 12:56:29 -0300

    refactor: separates code into layers

--- src/App.ts
+++ src/App.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import validationMiddleware from './Middlewares/validationMiddleware';
+import { validationResult } from 'express-validator';
 
 class App {
     public express: Express;
@@ -11,21 +12,17 @@ class App {
     }
 
     routes(): void {
-        this.express.route('/send').post([
-            body('name').notEmpty().isLength({ min: 3, max: 128 }),
-            body('phone').notEmpty().matches(/^\([0-9]{2}\) [0-9]?[0-9]{4}-[0-9]{4}$/),
-            body('email').notEmpty().isLength({ max: 128 }).isEmail(),
-            body('subject').notEmpty().isLength({ min: 8, max: 128 }),
-            body('message').notEmpty().isLength({ min: 20, max: 1200 })
-        ], (req: Request, res: Response) => {
-            const errors = validationResult(req);
+        this.express.route('/send').post(
+            validationMiddleware,
+            (req: Request, res: Response) => {
+                const errors = validationResult(req);
 
-            if (!errors.isEmpty()) {
-                return res.json({ errors: errors.array() });
-            }
+                if (!errors.isEmpty()) {
+                    return res.status(422).json({ error: 'Internal error in request.' });
+                }
 
-            res.send('Olá, mundo!');
-        });
+                res.send('Olá, mundo!');
+            });
     }
 }
 
---
+++ src/Middlewares/validationMiddleware.ts
@@ -0,0 +1,11 @@
+import { body, ValidationChain } from 'express-validator';
+
+const validationMiddleware: ValidationChain[] = [
+    body('name').notEmpty().isLength({ min: 3, max: 128 }),
+    body('phone').notEmpty().matches(/^\([0-9]{2}\) [0-9]?[0-9]{4}-[0-9]{4}$/),
+    body('email').notEmpty().isLength({ max: 128 }).isEmail(),
+    body('subject').notEmpty().isLength({ min: 8, max: 128 }),
+    body('message').notEmpty().isLength({ min: 20, max: 1200 })
+];
+
+export default validationMiddleware;
